d307c085a4875ae5ff317e85d2e2edb114debf86
compls159 <83001058+compls159@users.noreply.github.com>
2022-03-11 00:20:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,68 @@ db = client.mc10th
 app = Flask(__name__)
 
 # 메인 페이지
-@app.route('/main')
+@app.route('/main') #메인페이지 API
 def main():
     return render_template('mainPage.html')
+
+@app.route('/main') #생활용품 API
+def item():
+    return render_template('itemPage.html')
+
+@app.route('/main') #로그인 API
+def login():
+    return render_template('loginPage.html')
+
+#로그인 페이지
+@app.route('/login') #메인페이지 API
+def main():
+    return render_template('mainPage.html')
+
+@app.route('/login') #회원가입페이저 API
+def signup():
+    return render_template('signUpPage.html')
+
+#회원가입 페이지
+@app.route('/signup') #메인페이지API
+def signup():
+    return render_template('mainPage.html')
+
+#생활용품페이지
+@app.route('/life') #메인페이지API
+def main():
+    return render_template('mainPage.html')
+
+@app.route('/life') #이용방법API
+def goitem():
+    return render_template('itemPage.html')
+
+@app.route('/life')
+def gologin():
+    return render_template('loginPage.html')
+
+@app.route('/life', methods = ['GET'])
+def itemlist():
+    return
+
+@app.route('/life', methods = ['DELETE'])
+def delete():
+    return
+
+@app.route('/life', methods = ['GET'])
+def itemListModal():
+    return
+
+@app.route('/life', methods = ['GET'])
+def itemFilterModal():
+    return
+
+@app.route('/life',methods = ['GET'])
+def itemSelectModal():
+    return
+
+@app.route('/life', methods = ['POST'])
+def addItemModal():
+    return
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
