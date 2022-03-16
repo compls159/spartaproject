e89105ada92151b39ef84653352584a2a19d0180
migdracios <migdracios@gmail.com>
2022-03-16 22:44:30 +0900

    wip whole login

--- app.py
+++ app.py
@@ -14,12 +14,17 @@ app.secret_key = "mc11th_key"
 # 메인 페이지
 @app.route('/')  # 메인 페이지 API
 def main():
-    return render_template('mainPage.html')
-
+    if "user_id" in session:
+        return render_template('mainPage.html', login=True)
+    else:
+        return render_template('mainPage.html', login=False)
 
 @app.route('/item')  # 생활용품 페이지 API
 def item():
-    return render_template('itemPage.html')
+   if "user_id" in session:
+       return render_template('itemPage.html', login=True)
+   else:
+       return render_template('itemPage.html', login=False)
 
 
 @app.route('/login')  # 로그인 페이지 API
@@ -85,17 +90,17 @@ def signUpCheck():
     # 조건문 - 클라이언트 폼에서 작성되지 않은 항목 체크
     if id_receive == '' or pw_receive == '' or pw2_receive == '' or name_receive == '' or email_receive == '':
         flash('작성되지 않은 항목이 있습니다')
-        return redirect(url_for("home"))
+        return redirect(url_for("signUp"))
     else:
         # 조건문 - 비밀번호 일치 체크
         if pw_receive != pw2_receive:
             flash('비밀번호가 일치하지 않습니다')
-            return redirect(url_for("home"))
+            return redirect(url_for("signUp"))
         else:
             # 조건문 - DB조회 아이디 일치여부
             if id_receive == checkDB:
                 flash('이미 존재하는 ID입니다')
-                return redirect(url_for("home"))
+                return redirect(url_for("signUp"))
             else:
                 # Request 데이터 DB에 추가
                 doc = [
@@ -110,7 +115,7 @@ def signUpCheck():
                 # Session 추가
                 session['user_id'] = id_receive
                 flash('회원가입 완료되었습니다. 감사합니다')
-                return redirect(url_for("home"))
+                return redirect(url_for("signUp"))
 
 
 
